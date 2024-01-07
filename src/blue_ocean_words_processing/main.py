d85f66fb5a735db6bd93c5651a92244bcd6d97d2
gomader <gh970818@hotmain.com>
2024-01-07 22:17:04 +0800

    feat:整合数据处理流程

--- src/blue_ocean_words_processing/main.py
+++ src/blue_ocean_words_processing/main.py
@@ -1,5 +1,12 @@
+from blue_ocean_words_processing.read_raw_files import read_raw_files
+from blue_ocean_words_processing.related_words_processing import run as process
+from blue_ocean_words_processing.save_result import save_result
+
+
 def main():
-    ...
+    raw_data = read_raw_files()
+    data = process(raw_data=raw_data)
+    save_result(data=data)
 
 
 if __name__ == '__main__':
