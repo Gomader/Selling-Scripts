50a798c90cf0e55d03f55dd70453f6ec402ee73d
gomader <gh970818@hotmain.com>
2024-01-07 22:16:43 +0800

    feat:添加禁词与输出文件路径

--- src/blue_ocean_words_processing/config.py
+++ src/blue_ocean_words_processing/config.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from configs.config import Config
 
@@ -9,14 +10,17 @@ class BlueOceanWordQueryConfig:
     # 搜索人气
     SEARCH_POPULARITY = 500
     # 在线商品数
-    ONLINE_GOODS_NUMBER = 20000
+    ONLINE_GOODS_NUMBER = 10000
     # 转化率
     CONVERSION_RATE = 0.01
     # 直通车
-    SUBWAY_REFERENCE_PRICE = 2
+    SUBWAY_REFERENCE_PRICE = 1.5
     # 天猫占比
-    TIANMAO_RATE = 0.4
+    TIANMAO_RATE = 0.8
 
     WORD_ROOT_FILE = os.path.join(Config.BLUE_OCEAN_WORDS_PROCESSING_FOLDER, 'word_root.txt')
     OUTPUT_FOLDER = os.path.join(Config.BLUE_OCEAN_WORDS_PROCESSING_FOLDER, 'output')
     RAW_DATA_FOLDER = os.path.join(Config.BLUE_OCEAN_WORDS_PROCESSING_FOLDER, 'raw_data')
+    RESULT_FILE_NAME = os.path.join(OUTPUT_FOLDER, 'output.xlsx')
+
+    FORBIDDEN_WORDS = re.compile(r'三只松鼠|apm|美团|会员|香奈儿|西太后|成人|最|火影忍者|周大福')
