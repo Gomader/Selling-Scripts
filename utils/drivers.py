c1b1461797a1a0e4f583e46d7b23241b116477b9
gomader <gh970818@hotmain.com>
2024-01-07 13:59:10 +0800

    feat:添加selenium的driver选择器，并添加chrome的配置

---
+++ utils/drivers.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+
+def get_chrome_window():
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+    browser = webdriver.Chrome(options=chrome_options)
+
+    browser.maximize_window()
+
+    return browser
+
+
+def get_window(driver):
+    return {
+        "CHROME": get_chrome_window
+    }[driver]()
