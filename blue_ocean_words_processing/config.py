899a36b62eb14692b0c0591c75140b2612921a82
gomader <gh970818@hotmain.com>
2024-01-07 14:00:01 +0800

    feat:蓝海词筛选基础配置

---
+++ blue_ocean_words_processing/config.py
@@ -0,0 +1,11 @@
+class Config:
+    # 搜索人气
+    SEARCH_POPULARITY = 500
+    # 在线商品数
+    ONLINE_GOODS_NUMBER = 20000
+    # 转化率
+    CONVERSION_RATE = 0.01
+    # 直通车
+    SUBWAY_REFERENCE_PRICE = 2
+    # 天猫占比
+    TIANMAO_RATE = 0.4
